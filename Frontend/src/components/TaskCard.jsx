0a8be6555f846c59e8902a8531142927aa4400c9
Kartikeya <janardanagnihotram@gmail.com>
2025-12-11 17:10:40 +0530

    Lifted task state to Today page,added parent logic

--- Frontend/src/components/TaskCard.jsx
+++ Frontend/src/components/TaskCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function TaskCard({title , description , status}) {
+export default function TaskCard({title , description , status , onToggle }) {
     return (
         <div style={{
             border:"1px solid #ccc",
@@ -11,7 +11,7 @@ export default function TaskCard({title , description , status}) {
             <h3>{title}</h3>
             <p>{description}</p>
             <p>Status:{status ? "Done" : "Not Done" }</p>
-            <button onClick={() => Set(!status)}>
+            <button onClick={onToggle}>
                 {status ? "Undo" : "Mark as Done"}
             </button>
         </div>
--- Frontend/src/pages/Today.jsx
+++ Frontend/src/pages/Today.jsx
@@ -1,31 +1,42 @@
-import TaskCard from "../components/Taskcard"
-const tasks = [
-  {
-    title: "Learn React State",
-    description: "Understand useState and component reactivity"
-  },
-  {
-    title: "Solve 1 DSA Problem",
-    description: "Build consistency for placements"
-  },
-  {
-    title: "Work on Consistency App",
-    description: "Daily small progress + commit"
-  }
-]
+import { useState } from "react"
+
 export default function Today() {
+  const [tasks, setTasks] = useState([
+    {
+      title: "Learn React State",
+      description: "Understand useState and component reactivity",
+      status: false
+    },
+    {
+      title: "Solve 1 DSA Problem",
+      description: "Build consistency for placements",
+      status: false
+    },
+    {
+      title: "Work on Consistency App",
+      description: "Daily small progress + commit",
+      status: false
+    },
+    function toggleStatus(index) {
+    const updated = [...tasks]
+    updated[index].status = !updated[index].status
+    setTasks(updated)
+      }
+  ])
     return (
         <>
-            <h1> Today </h1>
+          <h1> Today </h1>
             
-            {tasks.map((task, index) => (
-                    <TaskCard 
-                        key={index}
-                        title={task.title}
-                        description={task.description}
-                    />
+          {tasks.map((task, index) => (
+              <TaskCard
+                key={index}
+                title={task.title}
+                description={task.description}
+                status={task.status}
+                onToggle={() => toggleStatus(index)}
+              />
             ))}
 
+
         </>
-    )
-}
+    )}
